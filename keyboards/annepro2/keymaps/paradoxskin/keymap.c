f5291856a778dc2d9241b769e0a2c4a7680534b2
paradoxskin <1312269430@qq.com>
2023-07-11 18:56:43 +0800

    try to add TO layer to annepro2(not test)

--- keyboards/annepro2/keymaps/paradoxskin/keymap.c
+++ keyboards/annepro2/keymaps/paradoxskin/keymap.c
@@ -4,6 +4,9 @@ enum anne_pro_layers {
   _BASE_LAYER,
   _FN1,
   _FN2,
+  _FN3,
+  _TN1,
+  _TN2,
 };
 
 // Key symbols are based on QMK. Use them to remap your keyboard
@@ -35,36 +38,63 @@ enum anne_pro_layers {
 */
 enum {
     TD_C_CE,
+    TD_R_BK,
 };
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_C_CE] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, C(KC_E))
+    [TD_C_CE] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, C(KC_E)),
+    [TD_R_BK] = ACTION_TAP_DANCE_LAYER_MOVE(KC_RIGHT, WIN_BASE)
 };
 
- const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_BASE_LAYER] = LAYOUT_60_ansi( /* Base */
-    KC_GRAVE,         KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-    KC_TAB,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-    LT(_FN1,KC_ESC),  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
-    KC_LSFT,                    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            RSFT_T(KC_UP),
-    TD(TD_C_CE),      MO(_FN2),          KC_LGUI, MT(MOD_LALT,KC_SPC),                KC_RALT, LT(_FN1,KC_LEFT),  LT(_FN2,KC_DOWN),   RCTL_T(KC_RIGHT)
-),
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE_LAYER] = LAYOUT_60_ansi( /* Base */
+        KC_GRAVE,         KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        LT(_FN1,KC_ESC),  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,                    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            RSFT_T(KC_UP),
+        TD(TD_C_CE),      MO(_FN2),          KC_LGUI, MT(MOD_LALT,KC_SPC),                KC_RALT, LT(_FN1,KC_LEFT),  LT(_FN2,KC_DOWN),   LT(_FN3,KC_RIGHT)
+    ),
 
- [_FN1] = LAYOUT_60_ansi( /* Base */
-    KC_AP2_BT_UNPAIR, KC_AP2_BT1,  KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,          KC_BTN1,     KC_MS_U,    KC_BTN2,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  RGB_HUI,  RGB_SAI,  RGB_VAI,
-    XXXXXXX,          KC_MS_L,     KC_MS_D,    KC_MS_R,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  RGB_SPI,            RGB_MOD,
-    KC_LSFT,                       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            RGB_TOG,
-    XXXXXXX,          XXXXXXX,     XXXXXXX,                                     XXXXXXX,                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
-),
+    [_FN1] = LAYOUT_60_ansi( /* Base */
+        KC_AP2_BT_UNPAIR, KC_AP2_BT1,  KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,          KC_BTN1,     KC_MS_U,    KC_BTN2,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  RGB_HUI,  RGB_SAI,  RGB_VAI,
+        XXXXXXX,          KC_MS_L,     KC_MS_D,    KC_MS_R,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  RGB_SPI,            RGB_MOD,
+        KC_LSFT,                       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            RGB_TOG,
+        XXXXXXX,          XXXXXXX,     XXXXXXX,                                     XXXXXXX,                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+    ),
 
- [_FN2] = LAYOUT_60_ansi( /* Base */
-    KC_WH_U, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   XXXXXXX,
-    KC_WH_D, KC_WH_L,  KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_HOME,  KC_END,   XXXXXXX,
-    KC_VOLD, KC_VOLU,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXXXXX,  XXXXXXX,            XXXXXXX,
-    KC_MPRV,           KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            XXXXXXX,
-    KC_MUTE, XXXXXXX,  KC_MPLY,                            XXXXXXX,                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
- ),
+    [_FN2] = LAYOUT_60_ansi( /* Base */
+        KC_WH_U, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   XXXXXXX,
+        KC_WH_D, KC_WH_L,  KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_HOME,  KC_END,   XXXXXXX,
+        KC_VOLD, KC_VOLU,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXXXXX,  XXXXXXX,            XXXXXXX,
+        KC_MPRV,           KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            XXXXXXX,
+        KC_MUTE, XXXXXXX,  KC_MPLY,                            XXXXXXX,                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+    ),
+
+    [_FN3] = LAYOUT_ansi_61(
+        TO(_TN2), _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,
+        _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,
+        _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
+        _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
+        TO(_TN1), _______,  _______,                            _______,                            _______,  _______,  _______,  _______
+    ),
+
+    // TO layer
+    [_TN1] = LAYOUT_ansi_61(
+        KC_TAB,  KC_ENT,   KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,
+        KC_7,    KC_8,     KC_9,    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,
+        KC_4,    KC_5,     KC_6,    _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
+        KC_1,              KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
+        KC_0,    KC_0,     KC_0,                               _______,                            _______,  _______,  _______,  TD(TD_R_BK)
+    ),
+
+    [_TN2] = LAYOUT_ansi_61(
+        _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
+        _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
+        _______, _______,  _______,                            _______,                            _______,  _______,  _______,  TD(TD_R_BK)
+    ),
 };
 const uint16_t keymaps_size = sizeof(keymaps);
 
