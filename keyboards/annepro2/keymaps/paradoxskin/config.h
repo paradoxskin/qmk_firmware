be10d6d2b7b5db24904fac93d90b16e8e5826f47
paradoxskin <1312269430@qq.com>
2023-07-13 22:27:04 +0800

    TAPPING_TERM more

--- keyboards/annepro2/keymaps/paradoxskin/config.h
+++ keyboards/annepro2/keymaps/paradoxskin/config.h
@@ -1,5 +1,5 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
 #define PERMISSIVE_HOLD
 
-#define TAPPING_TERM 180
+#define TAPPING_TERM 205
 #define TAPPINT_TERM_PER_KEY
